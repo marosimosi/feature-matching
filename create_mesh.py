8f627bfb59301c42fd3818bf71d29eedd8585e4a
marosimosi <marosimosi@gmail.com>
2023-05-30 16:41:07 +0300

    switch to horse

--- create_mesh.py
+++ create_mesh.py
@@ -6,34 +6,24 @@ import utility as U
 
 
 # Load the point cloud 
-pcd = o3d.io.read_point_cloud("ioulia.ply")
-
+pcd = o3d.io.read_point_cloud("barbara.ply")
 
 # normals needed for BPA
-pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=1.1, max_nn=30))
-pcd.orient_normals_consistent_tangent_plane(10)
 # o3d.visualization.draw_geometries([pcd], point_show_normal=True)
 
 # Ball Pivoting Algorithm with multiple radii
-radii = [0.00001, 0.00005, 0.0001, 0.0002, 0.0005, 0.0007, 0.001, 0.002, 0.004, 0.006, 0.008, 0.01, 0.02, 0.03] #0.01,0.02
+radii = [0.001, 0.002, 0.005, 0.01, 0.02, 0.04]
 mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
+# o3d.visualization.draw_geometries([mesh])
 
 # save the mesh
-o3d.io.write_triangle_mesh("ioulia.obj", mesh)
+o3d.io.write_triangle_mesh("barbara.obj", mesh)
 
 # for later use
 vertices = np.asarray(mesh.vertices)
 triangles = np.asarray(mesh.triangles)
-
-
-
-# -----------NORMALS------------------
-
 mesh.compute_vertex_normals()
-# visualize 
-# !!!  Ctrl + 9 to show normals
-# o3d.visualization.draw_geometries([mesh])
-
+mesh.compute_triangle_normals()
 
 
 
@@ -49,12 +39,12 @@ color_map = plt.cm.viridis
 colors = color_map(norm)
 mesh.vertex_colors = o3d.utility.Vector3dVector(colors[:, :3])
 # visualize
-# o3d.visualization.draw_geometries([mesh])
+# o3d.visualization.draw_geometries([mesh])       # !!!  CTRL + 9 to show normals, CTRL + 1 to return to curvature
+
 
 
 # -----------SALIENCY------------------
 
-mesh.compute_triangle_normals()
 # compute face areas
 face_areas = []
 for face in triangles:
