f6c176d0f3289967f84f0ea91fa949b86f1b64fd
marosimosi <marosimosi@gmail.com>
2023-06-01 00:57:24 +0300

    isotropic remeshing

--- create_mesh.py
+++ create_mesh.py
@@ -1,82 +1,119 @@
 import open3d as o3d
 import numpy as np
-import trimesh
+import pymeshlab
 import matplotlib.pyplot as plt
 import utility as U
+import pymeshlab as ml
+
+
+# ------------------------SIGNATURES-------------------------------
+def curvature(mesh, vertices, triangles):
+    # calculate delta coordinates
+    delta = U.delta_coordinates(vertices, triangles)
+    # calculate norm of delta vector
+    norm = np.sqrt((delta * delta).sum(-1))
+    norm = (norm - norm.min()) / (norm.max() - norm.min())
+    # colormap
+    color_map = plt.cm.viridis
+    colors = color_map(norm)
+    mesh.vertex_colors = o3d.utility.Vector3dVector(colors[:, :3])
+    # visualize
+    o3d.visualization.draw_geometries([mesh])       # !!!  CTRL + 9 to show normals, CTRL + 1 to return to curvature
+
+
+def saliency(mesh, vertices, triangles):
+    # compute face areas
+    face_areas = []
+    for face in triangles:
+        p0, p1, p2 = vertices[face]
+        v0, v1, v2 = p1 - p0, p2 - p0, p2 - p1
+        cross_product = np.cross(v0, v1)
+        area = 0.5 * np.linalg.norm(cross_product)
+        face_areas.append(area)
+
+    # get adjacency matrix
+    adjacency_matrix = U.adjacency_matrix_sparse(triangles)
+
+    # compute vertex saliency
+    vertex_saliency = []
+    for vertex_id in range(len(vertices)):
+        saliency = 0.0
+
+        neighbor_indices = adjacency_matrix[vertex_id].nonzero()[1]
+        for neighbor_id in neighbor_indices:
+            neighbor_normal = mesh.vertex_normals[neighbor_id]
+            vertex_normal = mesh.vertex_normals[vertex_id]
+            angle = np.arccos(np.dot(neighbor_normal, vertex_normal))
+            saliency += angle * face_areas[neighbor_id]
+        vertex_saliency.append(saliency)
+
+    # normalize
+    max_saliency = max(vertex_saliency)
+    min_saliency = min(vertex_saliency)
+    normalized_saliency = [(s - min_saliency) / (max_saliency - min_saliency) for s in vertex_saliency]
+    # colormap
+    color_map = plt.cm.viridis
+    colors = color_map(normalized_saliency)
+    mesh.vertex_colors = o3d.utility.Vector3dVector(colors[:, :3])
+    # visualize
+    o3d.visualization.draw_geometries([mesh])
+# -------------------------------------------------------------------
+
+
 
 
 # Load the point cloud 
 pcd = o3d.io.read_point_cloud("barbara.ply")
-
-# normals needed for BPA
-# o3d.visualization.draw_geometries([pcd], point_show_normal=True)
 
 # Ball Pivoting Algorithm with multiple radii
 radii = [0.001, 0.002, 0.005, 0.01, 0.02, 0.04]
 mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(pcd, o3d.utility.DoubleVector(radii))
 # o3d.visualization.draw_geometries([mesh])
 
-# save the mesh
-o3d.io.write_triangle_mesh("barbara.obj", mesh)
-
 # for later use
-vertices = np.asarray(mesh.vertices)
-triangles = np.asarray(mesh.triangles)
+mesh_vertices = np.asarray(mesh.vertices)
+mesh_triangles = np.asarray(mesh.triangles)
 mesh.compute_vertex_normals()
 mesh.compute_triangle_normals()
 
-
-
-# -----------CURVATURE----------------
-
-# calculate delta coordinates
-delta = U.delta_coordinates(vertices, triangles)
-# calculate norm of delta vector
-norm = np.sqrt((delta * delta).sum(-1))
-norm = (norm - norm.min()) / (norm.max() - norm.min())
-# colormap
-color_map = plt.cm.viridis
-colors = color_map(norm)
-mesh.vertex_colors = o3d.utility.Vector3dVector(colors[:, :3])
-# visualize
-# o3d.visualization.draw_geometries([mesh])       # !!!  CTRL + 9 to show normals, CTRL + 1 to return to curvature
+# save the mesh
+o3d.io.write_triangle_mesh("barbara.obj", mesh)
 
 
 
-# -----------SALIENCY------------------
 
-# compute face areas
-face_areas = []
-for face in triangles:
-    p0, p1, p2 = vertices[face]
-    v0, v1, v2 = p1 - p0, p2 - p0, p2 - p1
-    cross_product = np.cross(v0, v1)
-    area = 0.5 * np.linalg.norm(cross_product)
-    face_areas.append(area)
+# ISOTROPIC REMESHING
+ms = pymeshlab.MeshSet()
 
-# get adjacency matrix
-adjacency_matrix = U.adjacency_matrix_sparse(triangles)
+ms.load_new_mesh("barbara.obj")
+ms.apply_filter("meshing_isotropic_explicit_remeshing")
+ms.save_current_mesh("barbara_remeshed.obj")
 
-# compute vertex saliency
-vertex_saliency = []
-for vertex_id in range(len(vertices)):
-    saliency = 0.0
-    neighbor_indices = adjacency_matrix[vertex_id].nonzero()[1]
-    for neighbor_id in neighbor_indices:
-        neighbor_normal = mesh.vertex_normals[neighbor_id]
-        vertex_normal = mesh.vertex_normals[vertex_id]
-        angle = np.arccos(np.dot(neighbor_normal, vertex_normal))
-        saliency += angle * face_areas[neighbor_id]
-    vertex_saliency.append(saliency)
+# load the remeshed mesh
+remesh = o3d.io.read_triangle_mesh("barbara_remeshed.obj")
 
-# normalize
-max_saliency = max(vertex_saliency)
-min_saliency = min(vertex_saliency)
-normalized_saliency = [(s - min_saliency) / (max_saliency - min_saliency) for s in vertex_saliency]
-# colormap
-color_map = plt.cm.viridis
-colors = color_map(normalized_saliency)
-mesh.vertex_colors = o3d.utility.Vector3dVector(colors[:, :3])
-# visualize
-o3d.visualization.draw_geometries([mesh])
+
+# for later use
+remesh_vertices = np.asarray(remesh.vertices)
+remesh_triangles = np.asarray(remesh.triangles)
+remesh.compute_vertex_normals()
+remesh.compute_triangle_normals()
+
+# COMPARE
+o3d.visualization.draw_geometries([mesh], mesh_show_wireframe=True)
+o3d.visualization.draw_geometries([remesh], mesh_show_wireframe=True)
+
+curvature(mesh, mesh_vertices, mesh_triangles)
+curvature(remesh, remesh_vertices, remesh_triangles)
+
+saliency(mesh, mesh_vertices, mesh_triangles)
+saliency(remesh, remesh_vertices, remesh_triangles)
+
+
+
+
+
+
+
+
 
--- utility.py
+++ utility.py
@@ -66,3 +66,75 @@ def sample_colormap(scalars):
     colors = colormap(scalars)
 
     return colors[:,:-1]
+
+
+
+
+
+
+
+""" def have_common_edge(tri1, tri2):
+
+    common = 0
+    for i in tri1:
+        for j in tri2:
+            if i == j:
+                common += 1
+
+    if common == 2:
+        return True
+    else:
+        return False
+
+
+def circumcircle(triangle, vertices):
+
+    v1, v2, v3 = vertices[triangle]
+
+    A = np.array([[v2[0] - v1[0], v2[1] - v1[1], v2[2] - v1[2]],
+                  [v3[0] - v1[0], v3[1] - v1[1], v3[2] - v1[2]],
+                  [v3[0] - v2[0], v3[1] - v2[1], v3[2] - v2[2]]])
+
+    b = 0.5 * np.array([np.dot(v2, v2) - np.dot(v1, v1),
+                       np.dot(v3, v3) - np.dot(v1, v1),
+                       np.dot(v3, v3) - np.dot(v2, v2)])
+
+    try:
+        center = np.linalg.solve(A, b)
+        radius = np.sqrt(np.dot(v1 - center, v1 - center))
+        return center, radius
+    except np.linalg.LinAlgError:
+        # Handle the case when A is a singular matrix (degenerate triangle)
+        return None, None
+
+    return center, radius
+
+
+def non_common_vertex(tri1, tri2):
+    for i in tri1:
+        if i not in tri2:
+            return i
+
+
+def flip_edge(tri1, tri2, vertices, triangles):
+    v1 = non_common_vertex(tri1, tri2)
+    v2 = non_common_vertex(tri2, tri1)
+
+    # v3,v4 are the vertices of the triangles that are not v1 and v2
+    v3,v4 = [x for x in tri1 if x != v1 and x != v2]
+    
+    new_tri1 = [v1, v2, v3]
+    new_tri2 = [v1, v2, v4]
+
+    triangles = np.delete(triangles, np.where((triangles == tri1).all(axis=1))[0][0], axis=0)
+    triangles = np.delete(triangles, np.where((triangles == tri2).all(axis=1))[0][0], axis=0)
+    triangles = np.append(triangles, [new_tri1], axis=0)
+    triangles = np.append(triangles, [new_tri2], axis=0)
+
+    return triangles
+
+def collinear(v1, v2, v3):
+    if np.linalg.norm(np.cross(v2 - v1, v3 - v1)) == 0:
+        return True
+    else:
+        return False """
